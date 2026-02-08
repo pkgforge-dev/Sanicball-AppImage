b5bfd1dd5e633fd0f6d0bc8ccc52238c17f4fe33
Link4Electronics <link4electronics@gmail.com>
2026-02-07 21:50:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    gcr      \
     gd       \
     libdecor \
     libxaw   \
