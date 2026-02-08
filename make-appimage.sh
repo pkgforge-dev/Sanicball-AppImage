ac0df25f97c8ceb7736b35aa11b46b25fba17553
Link4Electronics <link4electronics@gmail.com>
2026-02-08 05:10:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/Sanicball_v0.8.2_Data
+quick-sharun /usr/bin/Sanicball_v0.8.2.x86_64 /usr/bin/Sanicball_v0.8.2_Data
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
