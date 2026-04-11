1933b8d8125ad50fad43a04ff052254f5d608c62
Link4Electronics <link4electronics@gmail.com>
2026-04-11 19:59:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,14 +6,16 @@ ARCH=$(uname -m)
 VERSION=0.8.2
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
-export DEPLOY_SDL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/Sanicball_v0.8.2.x86_64
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
