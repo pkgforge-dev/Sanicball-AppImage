e22b66eb03497541751b6d40013bebb9bc08da73
Link4Electronics <link4electronics@gmail.com>
2026-05-07 06:26:42 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor \
-    gnu-free-fonts
+pacman -Syu --noconfirm gnu-free-fonts
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
 wget https://archive.org/download/sanicball-archive/Sanicball%20Archive/Game%20versions/Linux/sanicball-v0.8.2-linux.zip
 bsdtar -xvf ./sanicball-v0.8.2-linux.zip
-rm -f *.zip
+rm -f ./*.zip
 mkdir -p ./AppDir/bin
 mv -v Sanicball_v0.8.2_Data ./AppDir/bin
 chmod +x Sanicball_v0.8.2.x86_64
