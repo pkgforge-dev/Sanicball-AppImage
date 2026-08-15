982e95da3dad3df392445100954b43576d3871bf
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:29:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,3 +16,7 @@ quick-sharun ./AppDir/bin/Sanicball_v0.8.2.x86_64
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
