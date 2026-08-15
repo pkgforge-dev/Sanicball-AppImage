be5bfae51ee859c102ab9877a0aacd25fe7b1d61
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:28:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,6 @@ export DEPLOY_SDL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/Sanicball_v0.8.2.x86_64
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
